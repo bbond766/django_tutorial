1a04a0cd476bbb1faf46dd8ef2e951787fc7eac2
Bradley <bradleybond766@gmail.com>
2022-11-29 13:51:43 -0700

    front page can accept list item and display it

--- functional_tests.py
+++ functional_tests.py
@@ -23,33 +23,38 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         # Checks ability to add new to-do items
-        inputBox = self.browser.find_element(By.ID, 'id_new_item')
-        self.assertEqual(inputBox.get_attribute('placeholder'),
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),
                          'Enter a to-do item')
 
         # Creates new list item
-        inputBox.send_keys('Buy peacock feathers')
+        inputbox.send_keys('Buy peacock feathers')
 
         # adds list item to list and tests that it is there
-        inputBox.send_keys(Keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            'New to-do item did not appear in table'
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
-        # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
-        self.fail('Finish the test!')
+        # Add a second item to the list
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        # The page updates again, and now shows both items on her list
+        # Test to make sure both items are still there
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly',
+                      [row.text for row in rows])
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
+        self.fail('Finish the test!')
 
         # She visits that URL - her to-do list is still there.
 
