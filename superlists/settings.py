2031963915ec539abae92126e89a4b37adb3f32e
Bradley <bradleybond766@gmail.com>
2022-12-08 16:22:10 -0700

    fixing issues with pushing to heroku main

--- superlists/settings.py
+++ superlists/settings.py
@@ -79,8 +79,10 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 
 DATABASES = {
     'default': {
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '<database_name>',
         'USER': '<user_name>',
         'PASSWORD': '<password>',
         'HOST': 'localhost',
